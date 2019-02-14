d882173ad7ca9c160f424b916869d687abd717ec
aznable813 <aznable813@gmail.com>
2019-02-14 09:59:30 +0900

    It's been a while from Note9 on Feb 14th

--- Include_prac/app/src/main/java/com/Include_prac/MainActivity.java
+++ Include_prac/app/src/main/java/com/Include_prac/MainActivity.java
@@ -15,31 +15,39 @@ public class MainActivity extends Activity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-		setLSNRs();
-		//set listeners	
+        InitView();
+
     }
 	
 
 	private void setLSNRs(){
-
 
 		//set BD1 button1
 		findViewById(R.id.incl_BD1_btn1).setOnClickListener(new OnClickListener(){
 			public void onClick(View v){
 				TextView tv1 = findViewById(R.id.incl_BD3).findViewById(R.id.incl_BD1_txt1);
 				tv1.setText("Changed 1st!!");
+				Button BtnTxt2 = findViewById(R.id.incl_BD2_btn1);
+				BtnTxt2.setText("Initialize View");
 				TextView tv2 = findViewById(R.id.incl_BD1_txt2);
 				tv2.setText("Changed 2nd!!");
-				
 			}
-			
-			
 		});
-
+		
+		//set BD2 button1 Initialize
+		findViewById(R.id.incl_BD2_btn1).setOnClickListener(new OnClickListener(){
+			public void onClick(View v){
+				InitView();
+			}
+		});
+		
 	}
 	
-	
+	//Initial View
+	private void InitView(){
+		setContentView(R.layout.main);
+		setLSNRs();
+	}
 	
 	
 	
