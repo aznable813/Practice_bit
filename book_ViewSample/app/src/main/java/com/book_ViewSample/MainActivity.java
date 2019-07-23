9f518ff8138b23ec9404b07f7880cc69f4708e7b
aznable813 <aznable813@gmail.com>
2019-07-23 12:04:13 +0900

    Book Learning from Note9

---
+++ book_ViewSample/app/src/main/java/com/book_ViewSample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.book_ViewSample;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
